243e6303ff404ada3cfb7fb0d3180cef0a9a92c8
Antoine ZANARDI <antoine.zanardi.partner@decathlon.com>
2025-08-05 17:54:13 +0200

    feat: implement iterator exercises with error handling and capitalize functions

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -1,3 +1,5 @@
+use crate::DivisionError::{IntegerOverflow, NotDivisible};
+
 #[derive(Debug, PartialEq, Eq)]
 enum DivisionError {
     // Example: 42 / 0
@@ -11,14 +13,25 @@ enum DivisionError {
 // TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
-    todo!();
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    }
+    if a == i64::MIN && b == -1 {
+        return Err(IntegerOverflow);
+    }
+    if a % b != 0 {
+        return Err(NotDivisible);
+    }
+    Ok(a / b)
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
-fn result_with_list() {
+fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    let division_results = numbers.into_iter().map(|n| divide(n, 27)).collect();
+
+    Ok(division_results)
 }
 
 // TODO: Add the correct return type and complete the function body.
@@ -26,6 +39,8 @@ fn result_with_list() {
 fn list_of_results() {
     let numbers = [27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+
+    division_results.collect::<Vec<Result<i64, DivisionError>>>()
 }
 
 fn main() {
